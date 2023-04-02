8b4cae86ed0ff6199254b3ca73ddec7bbe8bb7cf
ghadynasrallah <66480611+ghadynasrallah@users.noreply.github.com>
2023-04-02 16:56:05 +0300

    Added GetMessage and DeleteMessage methods to IMessageStorage.cs

--- ChatService/Storage/IMessageStorage.cs
+++ ChatService/Storage/IMessageStorage.cs
@@ -7,4 +7,8 @@ public interface IMessageStorage
     public Task<List<Message>> EnumerateMessagesFromAGivenConversation(String conversationId);
 
     public Task PostMessageToConversation(Message message);
+
+    public Task<Message?> GetMessage(string conversationId, string messageId);
+
+    public Task<bool> DeleteMessage(string conversationId, string messageId);
 }
