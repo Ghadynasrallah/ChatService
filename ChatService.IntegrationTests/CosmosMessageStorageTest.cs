b66bf4c69bd5b705571e0a2e6a2dc655500551ea
ghadynasrallah <66480611+ghadynasrallah@users.noreply.github.com>
2023-04-02 19:05:09 +0300

    Added more tests to CosmosMessageStorageTest.cs

--- ChatService.IntegrationTests/CosmosMessageStorageTest.cs
+++ ChatService.IntegrationTests/CosmosMessageStorageTest.cs
@@ -3,10 +3,12 @@ using ChatService.Storage;
 using ChatService.Storage.Entities;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Assert = Xunit.Assert;
 
 namespace ChatService.Integrationtests;
 
-public class CosmosMessageStorageTest : IClassFixture<WebApplicationFactory<Program>>
+public class CosmosMessageStorageTest : IClassFixture<WebApplicationFactory<Program>>, IAsyncLifetime
 {
     private readonly IMessageStorage _store;
     
@@ -26,7 +28,7 @@ public class CosmosMessageStorageTest : IClassFixture<WebApplicationFactory<Prog
     public async Task DisposeAsync()
     {
         await _store.DeleteMessage(_message1.conversationId, _message1.messageId);
-        await _store.DeleteMessage(_message1.conversationId, _message1.messageId);
+        await _store.DeleteMessage(_message2.conversationId, _message2.messageId);
     }
 
     [Fact]
@@ -40,7 +42,7 @@ public class CosmosMessageStorageTest : IClassFixture<WebApplicationFactory<Prog
         Assert.Equal(_message1Flipped, await _store.GetMessage("mike_foo", _message1.messageId));
     }
 
-    [Theory]
+    [Xunit.Theory]
     [InlineData("", "hello", "foo", "message id", 1000)]
     [InlineData("  ", "hello", "foo", "message id", 1000)]
     [InlineData("foo_bar", null, "foo", "message id", 1000)]
@@ -55,5 +57,60 @@ public class CosmosMessageStorageTest : IClassFixture<WebApplicationFactory<Prog
         {
             await _store.PostMessageToConversation(conversation);
         });
+    }
+
+    [Fact]
+    public async Task GetAlreadyExistingMessages()
+    {
+        Message expectedAlreadyExistingMessage1 = new Message("TestMessage", "My name is foo!", "foo", "foo_bar", 2000);
+        Message expectedAlreadyExistingMessage1Flipped = new Message("TestMessage", "My name is foo!", "foo", "bar_foo", 2000);
+        Assert.Equal(expectedAlreadyExistingMessage1, await _store.GetMessage("foo_bar", "TestMessage"));
+        Assert.Equal(expectedAlreadyExistingMessage1Flipped, await _store.GetMessage("bar_foo", "TestMessage"));
+    }
+
+    [Fact]
+    public async Task GetNonExistingMessage()
+    {
+        Assert.Null(await _store.GetMessage("mike_bar", "test"));
+    }
+
+    [Fact]
+    public async Task DeleteExistingMessage()
+    {
+        await _store.PostMessageToConversation(_message1);
+        Assert.True(await _store.DeleteMessage(_message1.conversationId, _message1.messageId));
+        Assert.Null(await _store.GetMessage(_message1.conversationId, _message1.messageId));
+        Assert.Null(await _store.GetMessage("mike_foo", _message1.messageId));
+    }
+
+    [Fact]
+    public async Task DeleteNonExistingMessage()
+    {
+        Assert.False(await _store.DeleteMessage("mike_bar", "test"));
+    }
+
+    [Fact]
+    public async Task EnumerateMessagesFromAGivenConversation()
+    {
+        await _store.PostMessageToConversation(_message1);
+        await _store.PostMessageToConversation(_message2);
+
+        List<Message> expectedMessagesForFoo = new List<Message>()
+        {
+            _message1,
+            new Message(_message2.messageId, _message2.text, _message2.senderUsername, "foo_mike", _message2.unixTime)
+        };
+        
+        List<Message> expectedMessagesForMike = new List<Message>()
+        {
+            new Message(_message1.messageId, _message1.text, _message1.senderUsername, "mike_foo", _message1.unixTime),
+            _message2
+        };
+
+        var realMessagesForFoo = await _store.EnumerateMessagesFromAGivenConversation("foo_mike");
+        var realMessagesForMike = await _store.EnumerateMessagesFromAGivenConversation("mike_foo");
+        
+        CollectionAssert.AreEquivalent(expectedMessagesForFoo, realMessagesForFoo); 
+        CollectionAssert.AreEquivalent(expectedMessagesForMike, realMessagesForMike); 
     }
 }
